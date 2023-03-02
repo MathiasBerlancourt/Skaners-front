03093d812c0733660eda31146c94e6007785c06f
Moustapha Diakhaby <diakhabymous@gmail.com>
2023-03-02 12:24:57 +0100

    final skan check

--- containers/CameraScreen.js
+++ containers/CameraScreen.js
@@ -1,6 +1,16 @@
 import { View, Text } from "react-native";
+import { Camera } from "react-native-vision-camera";
+import { useEffect } from "react";
+import axios from "axios";
 
 const CameraScreen = () => {
+  useEffect(() => {
+    const cameraPermission = async () => {
+      await Camera.getCameraPermissionStatus();
+    };
+
+    cameraPermission();
+  }, []);
   return (
     <View>
       <Text>Je suis un CameraSCreen</Text>
